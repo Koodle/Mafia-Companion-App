7ab29c7b58813969afd319425d90163b2217636a
Kazim <kazimmanji@yahoo.co.uk>
2021-11-23 11:10:17 +0000

    Started working on a recycler view to display the lobbies

---
+++ app/src/main/java/com/example/mafiacompanionapp/HostLobbyActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mafiacompanionapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HostLobbyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_host_lobby)
+    }
+}
---
+++ app/src/main/java/com/example/mafiacompanionapp/lobbyAdapter.kt
@@ -0,0 +1,8 @@
+package com.example.mafiacompanionapp
+
+import android.widget.ListAdapter
+
+class lobbyAdapter : ListAdapter<>{
+
+
+}
